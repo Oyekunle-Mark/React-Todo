32a112634ecd791ee9d4be77dd47ee7ddc13a8f7
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-29 13:11:59 +0100

    The todo list component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,12 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+export default function TodoList({ activities }) {
+  return (
+    <ul>
+      {activities.map(activity => (
+        <Todo key={activity.id} {...activity} />
+      ))}
+    </ul>
+  );
+}

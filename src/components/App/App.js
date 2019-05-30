17377d558f5b6c67f968d2dd6c4f1bc8117fc493
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-30 12:06:02 +0100

    Moved the data to another file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,7 @@ import React from "react";
 import TodoList from "../TodoComponents/TodoList/TodoList";
 import TodoForm from "../TodoComponents/TodoForm/TodoForm";
 import "./App.css";
-
-const activityList = [
-  {
-    task: "Organize Garage",
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: "Bake Cookies",
-    id: 1528817084358,
-    completed: false
-  }
-];
+import activityList from './activityList';
 
 class App extends React.Component {
   constructor(props) {
---
+++ src/components/App/activityList.js
@@ -0,0 +1,14 @@
+const activityList = [
+  {
+    task: "Add new tasks with the form",
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    task: "Click on a task to mark it as completed",
+    id: 1559214034025,
+    completed: false
+  }
+];
+
+export default activityList;
